85c8771de78b1caa04889358acca197dccf94781
AranjuezY <38692562+AranjuezY@users.noreply.github.com>
2018-07-29 01:57:52 +0800

    Create playlist.js

---
+++ src/playlist.js
@@ -0,0 +1,24 @@
+const playlist = {
+	"result": {
+		"tracks": [
+			{
+				"name": "Sold Out",
+				"artists":{"name": "Hawk Nelson"},
+				"album": {"name": "Diamonds"},
+				"mp3Url": "http://sc1.111ttt.cn:8282/2018/1/03m/13/396131232171.m4a?tflag=1519095601&pin=6cd414115fdb9a950d827487b16b5f97#.mp3"
+			},
+			{
+				"name": "Stayin' Alive",
+				"artists":{"name": "Bee Gees"},
+				"album": {"name": "Their Greatest Hits: The Record"},
+				"mp3Url": "http://sc1.111ttt.cn:8282/2018/1/03m/13/396131228287.m4a?tflag=1519095601&pin=6cd414115fdb9a950d827487b16b5f97#.mp3"
+			},
+			{
+				"name": "Tonight",
+				"artists":{"name": "Ken Laszlo"},
+				"album": {"name": "Tonight / 1.2.3.4.5.6.7.8"},
+				"mp3Url": "http://sc1.111ttt.cn:8282/2018/1/03m/13/396131203208.m4a?tflag=1519095601&pin=6cd414115fdb9a950d827487b16b5f97#.mp3"
+			}
+		]
+	}
+}
